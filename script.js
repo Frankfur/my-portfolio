cc2bd52161562695ca9de5761736495b0dee54a3
Frank Si <sif89695@gmail.com>
2026-09-13 13:53:41 +0800

    feat: add JavaScript basics - variables, operators, conditionals, loops 260913

---
+++ script.js
@@ -0,0 +1,10 @@
+// let currentHour = new Date().getHours();
+// // console.log("当前时间是：" + currentHour);
+// if (currentHour <=12) {
+//   console.log("Good morning!");
+// } else if (currentHour <=18) {
+//   console.log("Good afternoon!");
+// } else {
+//   console.log("Good evening!");
+// }
+
